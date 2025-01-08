1e1e6f420cb5f0f92b0ce00405d8aa95b9b618a5
Alejandro Villanueva Fuentes <alej.villanueva@duocuc.cl>
2025-01-08 10:11:23 -0300

    start

---
+++ src/app/shared/components/custon-input/custon-input.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-custon-input',
+  templateUrl: './custon-input.component.html',
+  styleUrls: ['./custon-input.component.scss'],
+})
+export class CustonInputComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
